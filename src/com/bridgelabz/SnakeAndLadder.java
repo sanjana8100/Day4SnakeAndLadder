3f151b3a5975164738e597901eb78544975934c3
sanjana8100 <sanjanams812@gmail.com>
2023-03-04 00:52:56 +0530

    added: UC5

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,24 +5,25 @@ import java.util.Scanner;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game.");
-        int startPos = 0;
+        int dice=0;
         int player=0;
         int endPos =100;
-        System.out.println("Starting position is :- "+startPos);
+        int needToWin = endPos-player;
         while(player<=endPos) {
             System.out.println("Do you want to Roll a Dice: \n 1: YES \t 2: NO \t");
             Scanner sc = new Scanner(System.in);
             int WantToPlay = sc.nextInt();
             System.out.println("***********************************************");
             if (WantToPlay == 1) {
-                int dice = (int) Math.floor(Math.random() * 10) % 7;
+                dice = (int) Math.floor(Math.random() * 10) % 7;
+                System.out.println("Current Player Position is: "+player);
                 System.out.println("The number after rolling the dice: " + dice);
                 int check = (int) Math.floor(Math.random() * 10) % 3;
                 if (check == 1) {
-                    System.out.println("You got ladder! Moving ahead by "+dice+" positions.");
+                    System.out.println("You got ladder! Moving ahead by " + dice + " positions.");
                     player += dice;
                 } else if (check == 2) {
-                    System.out.println("You got Snake! Moving behind by "+dice+" positions.");
+                    System.out.println("You got Snake! Moving behind by " + dice + " positions.");
                     player -= dice;
                     if(player<0){
                         System.out.println("The Position will be less than 0. So Starting Again!!!");
@@ -31,11 +32,19 @@ public class SnakeAndLadder {
                 } else {
                     System.out.println("You got NO-Play! Thus, staying in the same position.");
                 }
-                System.out.println("Current player position is "+player);
-            }
-            else if(WantToPlay==2) {
+                System.out.println("Current New Player Position is " + player);
+                System.out.println("Number of position needed to win: "+needToWin);
+            } else if (WantToPlay == 2) {
                 break;
             }
+        }
+        if(player==endPos){
+            System.out.println("***********************************************************************************");
+            System.out.println("Winner!!!");
+        }
+        else if(player>endPos){
+            player-=dice;
+            System.out.println("Reached position greater than 100. Thus, Current New Player Position is "+player);
         }
     }
 }
