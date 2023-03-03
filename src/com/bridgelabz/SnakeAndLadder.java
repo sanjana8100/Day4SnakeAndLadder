02095edea739701a1f31359cccbb462e196da44f
sanjana8100 <sanjanams812@gmail.com>
2023-03-04 01:00:10 +0530

    added: UC7

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,24 +3,25 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class SnakeAndLadder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to snake and ladder game.");
-        int dice=0;
+    public int DiceRoll(char PlayerName){
+        int dice=0,check=1;
         int player=0;
         int endPos =100;
         int DiceRollCount=0;
+        int winner=0;
         int needToWin = endPos-player;
-        while(player<=endPos) {
-            System.out.println("Do you want to Roll a Dice: \n 1: YES \t 2: NO \t");
+        while(player<=endPos && check == 1) {
+            System.out.println("__________________________________________________________________________________________________");
+            System.out.println("PLAYER " +PlayerName+ ": \nDo you want to Roll a Dice: \n 1: YES \t 2: NO \t");
             Scanner sc = new Scanner(System.in);
             int WantToPlay = sc.nextInt();
             System.out.println("***********************************************");
             if (WantToPlay == 1) {
                 dice = (int) Math.floor(Math.random() * 10) % 7;
-                System.out.println("Current Player Position is: "+player);
+                System.out.println("Current Player "+PlayerName+" Position is: "+player);
                 System.out.println("The number after rolling the dice: " + dice);
                 DiceRollCount++;
-                int check = (int) Math.floor(Math.random() * 10) % 3;
+                check = (int) Math.floor(Math.random() * 10) % 3;
                 if (check == 1) {
                     System.out.println("You got ladder! Moving ahead by " + dice + " positions.");
                     player += dice;
@@ -34,20 +35,36 @@ public class SnakeAndLadder {
                 } else {
                     System.out.println("You got NO-Play! Thus, staying in the same position.");
                 }
-                System.out.println("Current New Player Position is " + player);
+                System.out.println("Current New Player "+PlayerName+" Position is " + player);
                 System.out.println("Number of position needed to win: "+needToWin);
             } else if (WantToPlay == 2) {
                 break;
             }
         }
         if(player==endPos){
-            System.out.println("Winner!!!");
+            winner=1;
+            System.out.println("***********************************************************************************");
             System.out.println("Number of times the dice was rolled to WIN the game: "+DiceRollCount);
         }
-        else{
+        else if(player>endPos){
             player-=dice;
             System.out.println("Reached position greater than 100. Thus, Current New Player Position is "+player);
         }
-
+        return winner;
     }
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder game.");
+        int WinA=0,WinB=0;
+        SnakeAndLadder playerA= new SnakeAndLadder();
+        SnakeAndLadder playerB= new SnakeAndLadder();
+        while(WinA==0 && WinB==0){
+            WinA=playerA.DiceRoll('A');
+            WinB=playerB.DiceRoll('B');
+        }
+        if(WinA==1)
+            System.out.println("\nPlayer A is the WINNER!!!\n");
+        else
+            System.out.println("\nPlayer B is the WINNER!!!\n");
+    }
+
 }
