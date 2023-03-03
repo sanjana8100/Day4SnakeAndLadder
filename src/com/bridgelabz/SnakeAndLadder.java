4d3f8d31709166173555296098267cb868631198
sanjana8100 <sanjanams812@gmail.com>
2023-03-04 00:35:28 +0530

    added: UC1

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game.");
+        int startPos = 0;
+        System.out.println("Starting position is :- "+startPos);
     }
 }
