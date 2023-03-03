8e05f1487731d610b3dd691543b9726ee2a71234
sanjana8100 <sanjanams812@gmail.com>
2023-03-04 00:39:44 +0530

    added: UC2

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game.");
         int startPos = 0;
+        int dice = (int) Math.floor(Math.random()*10)%7;
         System.out.println("Starting position is :- "+startPos);
+        System.out.println("Dice roll number :- "+dice);
     }
 }
